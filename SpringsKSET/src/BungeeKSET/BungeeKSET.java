63b716a596111fa01dd918c4714c0f409484a305
c19ks <c19ks@dalton.org>
2018-01-11 10:43:36 -0500

    first commit

---
+++ SpringsKSET/src/BungeeKSET/BungeeKSET.java
@@ -0,0 +1,5 @@
+package BungeeKSET;
+
+public class BungeeKSET {
+	
+}
---
+++ SpringsKSET/src/BungeeKSET/spring.java
@@ -0,0 +1,15 @@
+package BungeeKSET;
+
+public class spring {
+	double k=0; //k constant
+	double m=0; //mass
+	double length=0; //length of spring
+	double displacement=0; //change in length
+	
+	public spring (double k, double m, double length, double displacement){
+		this.k=k;
+		this.m=m;
+		this.length=length;
+		this.displacement=displacement;
+	}
+}
